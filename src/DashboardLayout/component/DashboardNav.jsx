35905d43304b8725837536e34b5fcae2a9e09892
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2023-11-30 12:10:45 -0600

    update

--- src/DashboardLayout/component/DashboardNav.jsx
+++ src/DashboardLayout/component/DashboardNav.jsx
@@ -1,4 +1,4 @@
-import universitylogo from "../../assets/universitylogo.svg"
+import uni from "../../assets/uni.svg"
 import arrowdown from "../../assets/arrowdown.svg"
 import bell from "../../assets/bell.svg"
 
@@ -8,7 +8,7 @@ const DashboardNav = () => {
       <div className=" flex items-center p-6 gap-5">
         <img src={bell} alt="" />
         <div className="w-24 h-11 bg-neutral-100 flex justify-between p-2">
-          <img src={universitylogo} alt="" />
+          <img src={uni} alt="" />
           <img src={arrowdown} alt="" />
         </div>
       </div>
