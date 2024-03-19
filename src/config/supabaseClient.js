e7d007e95649372d68f7137c0509d873a8d6d103
George-Olufemi <georgeoluolufemi@gmail.com>
2024-03-19 14:05:26 +0100

    update

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ebktthjypikozcomiprt.supabase.co';
+const supabaseUrl = 'https://izctjgjzptzazmuubufy.supabase.co';
 const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVia3R0aGp5cGlrb3pjb21pcHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNTgyMzQsImV4cCI6MTk5MzkzNDIzNH0.HoPBO-EujbggQpgQIGv_UVIBS7kqKT303uQklOEojzQ';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6Y3RqZ2p6cHR6YXptdXVidWZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ3Nzk3ODksImV4cCI6MjAyMDM1NTc4OX0.fL0Hc3RtYmNBzBlm6KfjEMH3PyaiXLF0iUHzWZHb7E8';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
